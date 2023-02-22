7f8af5a5790d0cc7264ccfc559ae8310a96bfa0a
YunDamin <sctaemin@gmail.com>
2023-02-22 20:40:58 +0900

    Damin

---
+++ Web/frontend/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  devtool: "eval",
+  devServer: {
+    historyApiFallback: true,
+    port: process.env.DEV_PORT,
+    open: true,
+    hot: true,
+    devMiddleware: {
+      publicPath: "/", // 웹팩이 파일을 빌드해서 생성을 해주는 경로 , 가상의 경로 , dev server의 경우에는 메모리에 생성을 해준다
+    },
+  },
+
+  entry: {
+    app: path.join(__dirname, "src/index.tsx"),
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: ["babel-loader", "ts-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new RefreshWebpackPlugin(),
+    new Dotenv(),
+  ],
+};
