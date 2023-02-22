acbd05c1dafca8d6d76b35fb6b777e56284c3f1f
YunDamin <sctaemin@gmail.com>
2023-02-22 20:52:23 +0900

    save2

---
+++ Detector/detect.py
@@ -0,0 +1,84 @@
+import cv2
+import cvlib
+from cvlib.object_detection import draw_bbox
+
+import requests
+
+# API 정보
+api = "https://nysu-eufbm.run.goorm.site/api/exist_in_car/"
+response = requests.post(api, {
+    "license_plate": "1234",
+    "exist_state": "Camera send Connect"
+})
+
+# API 통신
+if response.status_code == 200:
+    print(f"API CONNECT! {response.status_code}")
+else:
+    print(f"Request failed with status code {response.status_code}") 
+    exit()
+
+cap = cv2.VideoCapture(0)
+
+# 정보
+car_num = 1234
+
+# 감지 상태
+baby_detect = False
+dog_detect = False
+
+while(True):
+    # frame 캡쳐
+    ret, frame = cap.read()
+
+    # 1 -> 좌우반전, 0 -> 상하반전
+    # frame = cv2.flip(frame, 1)
+
+    if not ret: break #프레임 오류
+    
+    # 흑백
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 감지
+    b, l, c = cvlib.detect_common_objects(frame, 0.5, 0.3, model="yolov8")
+
+    bbox = []
+    label = []
+    conf = []
+
+    current_baby_count = 0 # 현재 아기 수
+    current_dog_count = 0 # 현재 강아지 수
+    for i in range(len(l)):
+        if(l[i] == "person" or l[i] == "dog"):
+            bbox.append(b[i])
+            label.append(l[i])
+            conf.append(c[i])
+
+        if(l[i] == "person"): current_baby_count += 1
+        if(l[i] == "dog"): current_dog_count += 1
+
+    if "person" in label and baby_detect == False:
+        baby_detect = True
+        response = requests.post(api, data=data)
+    if "dog" in label and dog_detect == False:
+        dog_detect = True
+        response = requests.post(api, data=data)
+
+    if not "person" in label and baby_detect:
+        baby_detect = False
+        response = requests.post(api, data=data)
+    if not "dog" in label and dog_detect:
+        dog_detect = False
+        response = requests.post(api, data=data)
+
+
+    out = draw_bbox(frame, bbox, label, conf, write_conf=False)
+
+    # 실시간 보여주기
+    cv2.imshow("Baby Camera in Hyundai Car", out)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ Web/frontend/src/components/Main/MainPageContainer.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+// import axios from "axios";
+
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+
+import Button from "@mui/material/Button";
+
+import "./Main.scss";
+
+export default function MainPageContainer() {
+  const [data, setData] = React.useState({
+    license_plate: "",
+    name: "",
+    age: "",
+    address: "",
+    contact_number: "",
+    first_responder: "",
+    vehicle_type: "",
+    email: "",
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setData((data) => ({
+      ...data,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(data);
+  };
+
+  return (
+    <div className="MainPageContainer">
+      <div className="TitleContainer">
+        <form onSubmit={handleSubmit}>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={1}
+            width="100vw"
+            height="100vh"
+          >
+            <Grid item>
+              <Paper sx={{ width: "20vw", height: "5vh", borderRadius: 3 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="License Code"
+                  inputProps={{ "aria-label": "license_plate" }}
+                  onChange={handleInputChange}
+                />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper sx={{ width: "20vw", height: "5vh", borderRadius: 3 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Name"
+                  inputProps={{ "aria-label": "name" }}
+                  onChange={handleInputChange}
+                />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper sx={{ width: "20vw", height: "5vh", borderRadius: 3 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Age"
+                  inputProps={{ "aria-label": "age" }}
+                  onChange={handleInputChange}
+                />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper sx={{ width: "20vw", height: "5vh", borderRadius: 3 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Address"
+                  inputProps={{ "aria-label": "address" }}
+                  onChange={handleInputChange}
+                />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper sx={{ width: "20vw", height: "5vh", borderRadius: 3 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Phone Number"
+                  inputProps={{ "aria-label": "contact_number" }}
+                  onChange={handleInputChange}
+                />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper sx={{ width: "20vw", height: "5vh", borderRadius: 3 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Emergency Phone Number"
+                  inputProps={{ "aria-label": "first_responder" }}
+                  onChange={handleInputChange}
+                />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper sx={{ width: "20vw", height: "5vh", borderRadius: 3 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Vehicle Type"
+                  inputProps={{ "aria-label": "vehicle_type" }}
+                  onChange={handleInputChange}
+                />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Paper sx={{ width: "20vw", height: "5vh", borderRadius: 3 }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  placeholder="Email Address"
+                  inputProps={{ "aria-label": "email" }}
+                  onChange={handleInputChange}
+                />
+              </Paper>
+            </Grid>
+            <Grid item>
+              <Button
+                type="submit"
+                sx={{ color: "black", fontWeight: "bold", fontSize: "2vw" }}
+              >
+                Send
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </div>
+  );
+}
